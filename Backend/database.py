c8c8c57dc921fcf913bd4e68bf0fa35dfdaea65f
sidharth <sp@gmail.com>
2026-02-15 00:36:20 +0530

    db url update

--- Backend/database.py
+++ Backend/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
+import os
 from sqlalchemy.orm import sessionmaker,declarative_base
-DATABASE_URL="mysql://root:hxZjBNImsbxSRYZCGymKQMSRBzWmGEGh@metro.proxy.rlwy.net:34527/railway"
+DATABASE_URL=os.getenv("DATABASE_URL")
 engine=create_engine(DATABASE_URL)
 sessionLocal=sessionmaker(bind=engine)
 Base=declarative_base()
