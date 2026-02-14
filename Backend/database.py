4377bb0009fd6d907076a3afbdd02b6e1fb562b4
sidharth <sp@gmail.com>
2026-02-15 00:34:45 +0530

    db url update

--- Backend/database.py
+++ Backend/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
-DATABASE_URL="mysql+pymysql://root:12345@localhost:3306/jobportal"
+DATABASE_URL="mysql://root:hxZjBNImsbxSRYZCGymKQMSRBzWmGEGh@metro.proxy.rlwy.net:34527/railway"
 engine=create_engine(DATABASE_URL)
 sessionLocal=sessionmaker(bind=engine)
 Base=declarative_base()
