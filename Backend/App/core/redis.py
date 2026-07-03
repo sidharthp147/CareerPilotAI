7acb7c04e9e17a39b1c3d4c381945c0b2e55f20c
sidharth <sp@gmail.com>
2026-07-04 03:07:48 +0530

    redis update

--- Backend/App/core/redis.py
+++ Backend/App/core/redis.py
@@ -1,3 +1,9 @@
 import redis.asyncio as redis
 from core.config import REDIS_URL
-redis_client = redis.from_url(REDIS_URL,decode_responses=True)
+try:
+  redis_client = redis.from_url(REDIS_URL,decode_responses=True)
+  redis_client.ping()
+  print("Redis client initialized successfully.")
+except Exception as e:
+  print(f"Error initializing Redis client: {e}")
+  redis_client = None
