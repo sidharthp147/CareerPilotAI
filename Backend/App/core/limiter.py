25f8efdf6f1544689655ded8cef429d4825948c1
sidharth <sp@gmail.com>
2026-07-04 03:21:08 +0530

    limiter.py update

--- Backend/App/core/limiter.py
+++ Backend/App/core/limiter.py
@@ -1,10 +1,11 @@
 from slowapi.util import get_remote_address
 from slowapi import Limiter
 from fastapi import Request
+from core.config import REDIS_URL
 def user_or_ip(request:Request):
     user = getattr(request.state, "user_id", None)
     if user :
         return f"ratelimit:user:{user}"
     else:
         return f"ratelimit:ip:{get_remote_address(request)}"
-limiter = Limiter(key_func=user_or_ip,storage_uri="redis://localhost:6379")
+limiter = Limiter(key_func=user_or_ip,storage_uri=REDIS_URL)
