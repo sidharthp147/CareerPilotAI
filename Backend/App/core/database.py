1daed2a5ea1dbad2db075910a323c00a9c3fde1c
sidharth <sp@gmail.com>
2026-07-04 00:16:38 +0530

    databse update

--- Backend/App/core/database.py
+++ Backend/App/core/database.py
@@ -3,6 +3,12 @@ from core.config import DATABASE_URL
 from sqlalchemy.orm import sessionmaker,declarative_base
 DATABASE_URL=DATABASE_URL
 engine=create_engine(DATABASE_URL,connect_args={"ssl":{"ca":"core/ca.pem"}})
+try:
+    with engine.connect() as connection:
+        connection.execute("SELECT 1")
+        print("Database connection successful.")
+except Exception as e:
+    print(f"Database connection failed: {e}")
 sessionLocal=sessionmaker(bind=engine)
 Base=declarative_base()
 def get_db():
