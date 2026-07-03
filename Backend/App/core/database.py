3a927558b740d555e3c98daa1128dd663af35230
sidharth <sp@gmail.com>
2026-07-04 00:24:41 +0530

    databse update2

--- Backend/App/core/database.py
+++ Backend/App/core/database.py
@@ -1,11 +1,11 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from core.config import DATABASE_URL
 from sqlalchemy.orm import sessionmaker,declarative_base
 DATABASE_URL=DATABASE_URL
 engine=create_engine(DATABASE_URL,connect_args={"ssl":{"ca":"core/ca.pem"}})
 try:
     with engine.connect() as connection:
-        connection.execute("SELECT 1")
+        connection.execute(text("SELECT 1"))
         print("Database connection successful.")
 except Exception as e:
     print(f"Database connection failed: {e}")
